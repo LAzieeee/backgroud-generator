9998566883f5c6dc20a832a665af93d696a03abb
Traiboon Kirdsong <traiboon2007@gmail.com>
2022-06-01 12:51:30 +0700

    adding starting project

---
+++ bg-gen.js
@@ -0,0 +1,12 @@
+let css = document.querySelector('h3');
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2');
+
+setGradient = () => {
+    document.body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")"
+    css.textContent = document.body.style.background + ";"
+}
+
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
